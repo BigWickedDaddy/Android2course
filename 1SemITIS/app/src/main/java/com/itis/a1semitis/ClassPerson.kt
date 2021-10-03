6e32333cef95766650f42b67d457ccfe539aaad4
Oleg S <olegsolovyanenko@gmail.com>
2021-10-04 01:33:22 +0300

    Hw 4.10

--- 1SemITIS/app/src/main/java/com/itis/a1semitis/ClassPerson.kt
+++
@@ -1,8 +0,0 @@
-package com.itis.a1semitis
-
-open class Person constructor(
-    val name: String,
-    val personID:Int,
-) {
-
-}
--- 1SemITIS/app/src/main/java/com/itis/a1semitis/InterfaceStudy.kt
+++
@@ -1,5 +0,0 @@
-package com.itis.a1semitis
-
-interface InterfaceStudy {
-    fun Study();
-}
